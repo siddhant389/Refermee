3e70e91c8faf64b58633a614b2d68e4c369c1b5a
surbhi1jain <surbhi>
2015-06-30 16:52:43 +0530

    catagory

--- src/AppBundle/Resources/DAO/CatagoryDaoPDO.php
+++ src/AppBundle/Resources/DAO/CatagoryDaoPDO.php
@@ -42,7 +42,7 @@ class CatagoryDaoPDO extends BaseDaoPDO {
     }
 
     public function retrieveFullTree($objCatagory) {
-        $query = "SELECT node.catagory_name FROM catagory AS node, catagory AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.catagory_name = 'BOOKS'AND catagory_name = :CATAGORY_NAME ORDER BY node.lft";
+        $query = "SELECT node.catagory_name FROM catagory AS node, catagory AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.catagory_name = :CATAGORY_NAME ORDER BY node.lft";
         try {
             $dbconn = $this->getConnection();
             $stmt = $dbconn->prepare($query);
@@ -55,7 +55,7 @@ class CatagoryDaoPDO extends BaseDaoPDO {
     }
 
     public function retrieveLeafNodes($objCatagory) {
-        $query = "SELECT catagory_name FROM catagory WHERE rgt = lft +1 AND catagory_name = :CATAGORY_NAME";
+        $query = "SELECT catagory_name FROM catagory WHERE rgt = lft +1";
         try {
             $dbconn = $this->getConnection();
             $stmt = $dbconn->prepare($query);
@@ -81,7 +81,7 @@ class CatagoryDaoPDO extends BaseDaoPDO {
     }
     
     public function depthOfNodes($objCatagory){
-        $query = "SELECT node.catagory_name, (COUNT(parent.catagory_name) - 1) AS depth FROM catagory AS node, catagory AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND catagory_name = :CATAGORY_NAME GROUP BY node.catagory_name ORDER BY node.lft";
+        $query = "SELECT node.catagory_name, (COUNT(parent.catagory_name) - 1) AS depth FROM catagory AS node, catagory AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt  GROUP BY node.catagory_name ORDER BY node.lft";
         try{
              $dbconn = $this->getConnection();
             $stmt = $dbconn->prepare($query);
@@ -93,7 +93,7 @@ class CatagoryDaoPDO extends BaseDaoPDO {
         }
     }
     public static function addNodes($objCatagory) {
-        $query = "LOCK TABLE catagory WRITE; SELECT @myRight := rgt FROM catagory WHERE catagory_name = :CATAGORY_NAME; UPDATE catagory SET rgt = rgt + 2 WHERE rgt > @myRight; UPDATE catagory SET lft = lft + 2 WHERE lft > @myRight; INSERT INTO catagory(name, lft, rgt) VALUES('GAME CONSOLES', @myRight + 1, @myRight + 2); UNLOCK TABLES";
+        $query = "LOCK TABLE catagory WRITE; SELECT @myRight := rgt FROM catagory WHERE catagory_name = :CATAGORY_NAME; UPDATE catagory SET rgt = rgt + 2 WHERE rgt > @myRight; UPDATE catagory SET lft = lft + 2 WHERE lft > @myRight; INSERT INTO catagory(catagory_name, lft, rgt) VALUES('newCatagory', @myRight + 1, @myRight + 2); UNLOCK TABLES";
         try{
             $dbconn = $this->getConnection();
             $stmt = $dbconn->prepare($query);
@@ -106,7 +106,7 @@ class CatagoryDaoPDO extends BaseDaoPDO {
     }
 
     public static function deleteNodes ($objCatagory) {
-        $query = "LOCK TABLE catagory WRITE; SELECT @myLeft := lft, @myRight := rgt, @myWidth := rgt - lft + 1 FROM catagory WHERE catagory_name = :CATAGORY_NAME; DELETE FROM catagory WHERE lft BETWEEN @myLeft AND @myRight; UPDATE catagory SET rgt = rgt - @myWidth WHERE rgt > @myRight; UPDATE catagory SET lft = lft - @myWidth WHERE lft > @myRight; UNLOCK TABLES;";
+        $query = "LOCK TABLE catagory WRITE; SELECT @myLeft := lft, @myRight := rgt, @myWidth := rgt - lft + 1 FROM catagory WHERE catagory_name = :CATAGORY_NAME; DELETE FROM catagory WHERE lft BETWEEN @myLeft AND @myRight; UPDATE catagory SET rgt = rgt - @myWidth WHERE rgt > @myRight; UPDATE catagory SET lft = lft - @myWidth WHERE lft > @myRight; UNLOCK TABLES";
         try{
             $dbconn = $this->getConnection();
             $stmt = $dbconn->prepare($query);
