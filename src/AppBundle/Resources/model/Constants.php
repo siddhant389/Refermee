608f23c376e71b705d53fee9825859361a1c1f94
surbhi1jain <surbhi>
2015-07-08 13:56:17 +0530

    yeye

---
+++ src/AppBundle/Resources/model/Constants.php
@@ -0,0 +1,61 @@
+<?php
+namespace AppBundle\Resources\model;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Constants
+ *
+ * @author surbhi
+ */
+final class Constants {
+    //put your code here
+    const USERNAME = 'username';
+    const USER_ID = 'user_id';
+    const PASSWORD = 'password';
+    const EMAIL = 'email';
+    const INSTITUTE_NAME = 'institute_name';
+    const INSTITUTE_ID = 'institute_id';
+    const YEAR_OF_COMPLETION = 'year_of_completion';
+    const COURSE_ID = 'course_id';
+    const COURSE_NAME = 'course_name';
+    const SPECIFICATION_ID = 'specification_id';
+    const SPECIFICATION_NAME = 'specification_name';
+    const ORGANISATION = 'organisation';
+    const PROFESSION = 'profession';
+    const SPECIFICATION = 'specification';
+    const REGISTRATION_TIMESTAMP = 'registration_timestamp';
+    const CONTACT = 'contact';
+    const SUBS_TRANS_ID = 'subs_trans_id';
+    const SUBS_TYPE_ID = 'subs_type_id';
+    const SUBS_TIMESTAMP = 'subs_timestamp';
+    const PUB_ID = 'pub_id';
+    const PUB_USERNAME = 'pub_username';
+    const PUB_EMAIL = 'pub_email';
+    const PUB_PASSWORD = 'pub_password';
+    const ADDRESS = 'address';
+    const CITY = 'city';
+    const PINCODE = 'pincode';
+    const ORGANISATION_NAME = 'organisation_name';
+    const PPU_TRANS_ID = 'ppu_trans_id';
+    const PPU_TYPE_ID = 'ppu_type_id';
+    const PPU_TIMESTAMP = 'ppu_timestamp';
+    const PPU_TYPE = 'ppu_type';
+    const BOOK_ID = 'book_id';
+    const BOOK_NAME = 'book_name';
+    const ISBN_NO = 'isbn_no';
+    const NO_OF_PAGES = 'no_of_pages';
+    const PHYSICAL_PRICE = 'physical_price';
+    const DIGITAL_PRICE = 'digital_price';
+    const CATAGORY_ID = 'catagory_id';
+    const CATAGORY_NAME = 'catagory_name';
+    const LFT = 'lft';
+    const RGT = 'rgt';
+    const INVENTORY_LINK = 'inventory_link';
+    const AUTHOR_ID = 'author_id';
+    const AUTHOR_NAME = 'author_name';
+    const GENERE_NAME = 'genere_name';
+ }
